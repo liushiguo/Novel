357dfbfdca5bdca994d9c24b5fb7dc47c72b1b10
lts <liutangbei@gmail.com>
2018-02-23 16:56:28 +0800

    Initial Commit

---
+++ Novel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Novel
+//
+//  Created by user on 2018/2/23.
+//  Copyright © 2018年 LSG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Novel/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Novel
+//
+//  Created by user on 2018/2/23.
+//  Copyright © 2018年 LSG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
